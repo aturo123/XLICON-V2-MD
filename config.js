8db645fbaffb095bc5a1a46f11e1da152fac11f2
aturo123 <159647163+aturo123@users.noreply.github.com>
2024-02-11 03:28:44 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
+  ['923022700980', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['923022700980', '𝑳𝒆𝒈𝒆𝒏𝒅 𝑳𝒂𝒓𝒌𝒂', true], 
+  ['923022700980', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233531910067', 'Abraham Dwamena3', true],
   
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923022700980" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
+global.mods = ['923022700980', '94789481495', '923022700980','233531910067','233208179431'] 
+global.prems = ['923022700980','233208179431','94789481495', '923022700980','233531910067']
+global.allowed = ['923022700980', '233208179431', '94789481495', '923022700980','233531910067']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -55,10 +55,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = '𝑳𝒆𝒈𝒆𝒏𝒅 𝑳𝒂𝒓𝒌𝒂'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = '𝑳𝒆𝒈𝒆𝒏𝒅 𝑳𝒂𝒓𝒌𝒂' 
+global.author = '𝑳𝒆𝒈𝒆𝒏𝒅 𝑳𝒂𝒓𝒌𝒂' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
